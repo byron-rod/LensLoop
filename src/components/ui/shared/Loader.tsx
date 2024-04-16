d00bd37a03c88d4197c688101b29698af2ca00cd
Byron <byron303@gmail.com>
2024-04-16 10:56:14 -0600

    fixing loader error in deployment

--- src/components/ui/shared/Loader.tsx
+++ src/components/ui/shared/Loader.tsx
@@ -1,12 +1,9 @@
+import loader from "/public/assets/icons/loader.svg";
+
 const Loader = () => {
   return (
     <div className="flex-center w-full">
-      <img
-        src="../../../../public/assets/icons/loader.svg"
-        alt="loader"
-        width={24}
-        height={24}
-      />
+      <img src={loader} alt="loader" width={24} height={24} />
     </div>
   );
 };
